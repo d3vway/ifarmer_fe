f7826dbf6a01812fc4e0abb376a1b6151b0aeea7
Yusuf T420 <mhyusufibrahim@gmail.com>
2021-08-30 22:44:40 +0700

    First init VueJS+TS

---
+++ src/views/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an login page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Login extends Vue {}
+</script>
---
+++ src/views/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="about">
+    <h1>This is an register page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Register extends Vue {}
+</script>
